e6236e24a7a1a5b305cd3ec7abb869ae10866995
Willis220 <36093050+Willis220@users.noreply.github.com>
2024-05-09 21:13:28 -0500

    Update login.php
    Ajuste1

--- login.php
+++ login.php
@@ -2,21 +2,18 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Inicio de sesión exitoso</title>
+    <title>Inicio de sesión</title>
 </head>
 <body>
     <?php
-    // Verifica si se enviaron los datos del formulario
     if(isset($_GET['usuario']) && isset($_GET['contrasena'])) {
         $usuario = $_GET['usuario'];
         $contrasena = $_GET['contrasena'];
-        
-        // Muestra los datos ingresados
+
         echo "<h2>Datos ingresados:</h2>";
         echo "Usuario: " . $usuario . "<br>";
         echo "Contraseña: " . $contrasena . "<br>";
     } else {
-        // Redirecciona al formulario si no se enviaron los datos
         header("Location: index.html");
         exit();
     }
